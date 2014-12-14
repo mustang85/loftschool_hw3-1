2fc2d35f063f9ba0b5c304ad571e2f2f18472b11
Anton Cherepov <chekitdnb@gmail.com>
2014-12-14 13:29:35 +0300

    add gulp, rebuild

--- gulp/php.gulp.js
+++ gulp/php.gulp.js
@@ -1,14 +1,42 @@
-//Tasks to create PHP dirrectory and copy to WAMP dev folder
+'use strict';
 
+var gulp = require('gulp'),
+	del = require('del');
+
+//Tasks to create PHP dirrectory and copy to WAMP dev folder
 var paths = {
-	php: '',
-	dev: ''
+	css: 'css',
+	js: 'js',
+	images: 'images',
+	dev: 'C:\\wamp\\www\\wmapp\\',
+	app: 'serv/',
+	link: ''
 };
 
-gulp.task('php', function () {
+//Copy CSS
+gulp.task('csscopy', function () {
+	return gulp.src('dist/css/**/*')
+		.pipe(gulp.dest(paths.link + paths.css));
+});
 
+//Copy JS
+// gulp.task('jscopy', function () {
+// 	return gulp.src('dist/js/**/*')
+// 		.pipe(gulp.dest(paths.js));
+// });
+
+//Copy Images
+gulp.task('imgcopy', function () {
+	return gulp.src('dist/images/**/*')
+		.pipe(gulp.dest(paths.link + paths.images));
+});
+
+gulp.task('php', function () {
+	paths.link = paths.app;
+	gulp.start('csscopy', 'imgcopy');
 });
 
 gulp.task('dev', function () {
-	gulp.src()
+	paths.link = paths.dev;
+	gulp.start('csscopy', 'imgcopy');
 });
