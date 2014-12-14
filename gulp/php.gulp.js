b938f40e12f840e4c2da527969d7c6aed85aed38
Anton Cherepov <chekitdnb@gmail.com>
2014-12-14 12:01:42 +0300

    add gulp build for php

---
+++ gulp/php.gulp.js
@@ -0,0 +1,14 @@
+//Tasks to create PHP dirrectory and copy to WAMP dev folder
+
+var paths = {
+	php: '',
+	dev: ''
+};
+
+gulp.task('php', function () {
+
+});
+
+gulp.task('dev', function () {
+	gulp.src()
+});
